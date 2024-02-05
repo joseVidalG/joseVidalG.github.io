b6e3da4501bf7e2646ded050ce9b317872984a64
joseVidalG <90925034+joseVidalG@users.noreply.github.com>
2024-02-05 03:52:26 -0500

    newperfil

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,17 +3,24 @@ const rootHtml = document.documentElement
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 const menuLinks = document.querySelectorAll(".menu__link");
 
+
+/*-----*/ 
+
 function changeTheme(){
   const currentTheme = rootHtml.getAttribute("data-theme");
-
+  /*opc*/
   currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
 
-  toggleTheme.classList.toggle("bi-sun")
-  toggleTheme.classList.toggle("bi-moon-stars")
+  toggleTheme.classList.toggle("bi bi-brightness-high")
+  toggleTheme.classList.toggle("bi bi-brightness-high-fill")
 }
+
+
 
 toggleTheme.addEventListener("click", changeTheme);
 
+
+/**lista */
 accordionHeaders.forEach(header => {
   header.addEventListener("click", () => {
     const accordionItem = header.parentElement;
@@ -29,3 +36,47 @@ menuLinks.forEach(item => {
     item.classList.add("active");
   })
 })
+
+
+
+var words = ['Desarrollador frond-end ','  Soporte TI ','Linux','windows'],
+    wordWrapper = document.getElementById('word'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 1;
+    
+
+    
+setInterval(function(){
+  if(wordWrapperContent.length > 1 && !addingWord  ) {
+    wordWrapper.innerHTML = wordWrapperContent.slice(0, -3);
+    wordWrapperContent = wordWrapper.innerHTML;
+    
+    
+
+  } else {
+    addingWord = true;
+  }
+
+  if( addingWord ){
+    if( wordWrapperContent.length < words[counter].length  ) {
+      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 4);
+      wordWrapperContent = wordWrapper.innerHTML;
+    } else {
+      if( counter < words.length) {
+        counter ++
+      }
+      addingWord = false;
+    }
+  }
+
+  if( counter == words.length) {
+    counter = 0;
+    
+  }
+
+}, 200);
+
+
+
+
